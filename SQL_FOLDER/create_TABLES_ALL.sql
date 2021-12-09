97dcff19e795362b68993127790e32fc6f4213be
Chominhyun <simh3077@ajou.ac.kr>
2021-12-09 21:30:44 +0900

    add: new_sql for creates

---
+++ SQL_FOLDER/create_TABLES_ALL.sql
@@ -0,0 +1,92 @@
+-- create_TABLE 모두 총합
+
+-- 스타디움 테이블 생성
+CREATE TABLE STADIUM (
+Stadium_id INT UNSIGNED PRIMARY KEY,
+Stadium_name VARCHAR(45),
+Capacity VARCHAR(45)
+);
+
+-- 클럽_인포 테이블 생성
+CREATE TABLE CLUB_INFO(
+Club_id INT UNSIGNED PRIMARY KEY,
+Club_name VARCHAR(20),
+Stadium INT UNSIGNED NOT NULL,
+badge_images_url VARCHAR(100),
+club_url VARCHAR(100),
+FOREIGN KEY(Stadium) REFERENCES STADIUM(Stadium_id)
+);
+
+-- 선수 테이블 생성
+CREATE TABLE PLAYER(
+Player_id INT UNSIGNED PRIMARY KEY ,
+Club_id INT UNSIGNED NOT NULL,
+Player_name VARCHAR(45),
+Uniform_num INT,
+Date_of_birth VARCHAR(45),
+position VARCHAR(45),
+FOREIGN KEY(Club_id) REFERENCES CLUB_INFO(Club_id)
+);
+
+-- 경기 결과 테이블 생성
+CREATE TABLE MATCH_INFO
+(
+	match_id INT UNSIGNED primary key,
+    match_date char(8),
+    home_club_id INT UNSIGNED,
+    away_club_id INT UNSIGNED,
+    foreign key (home_club_id) references CLUB_INFO(Club_id),
+    foreign key (away_club_id) references CLUB_INFO(Club_id)
+);
+
+-- UPDATE new_schema.MATCH_INFO
+-- SET match_date = null
+-- WHERE match_date = "";
+
+CREATE TABLE MATCH_DETAIL(
+  Match_id INT UNSIGNED NOT NULL PRIMARY KEY,
+  Home_score INT NOT NULL,
+  Away_score INT NOT NULL,
+  King_of_the_match INT NOT NULL
+  );
+
+CREATE TABLE MATCH_WIN(
+  Match_id INT NOT NULL,
+  Winner_club_id INT NOT NULL
+);
+
+-- 클럽 결과 테이블 생성
+CREATE TABLE CLUB_RESULT(
+Club_id INT UNSIGNED PRIMARY KEY,
+position INT UNSIGNED,
+played INT UNSIGNED,
+won INT UNSIGNED,
+draw INT UNSIGNED,
+lost INT UNSIGNED,
+gf INT UNSIGNED,
+ga INT UNSIGNED,
+form VARCHAR(5),
+FOREIGN KEY(Club_id) REFERENCES CLUB_INFO(Club_id)
+);
+
+CREATE TABLE GOAL_OF(
+	match_id INT UNSIGNED NOT NULL, -- USING UNSIGNED INT, SET FOREIGN KEY
+    club_id INT UNSIGNED NOT NULL, -- USING UNSIGNED TINYINT, SET FOREIGN KEY
+    player_id INT UNSIGNED NOT NULL, -- USING UNSIGNED INT
+    goal_id INT UNSIGNED PRIMARY KEY, -- USING UNSIGNED INT, SET PK
+    goal_time CHAR(10), -- USING UNSIGNED INT
+    FOREIGN KEY(match_id) REFERENCES MATCH_INFO(Match_id), -- USING MATCH_DETAIL.match_id FOR FOREIGN KEY
+    FOREIGN KEY(player_id) REFERENCES PLAYER(player_id), -- USING PLAYER.player_id FOR FOREIGN KEY
+    FOREIGN KEY(club_id) REFERENCES PLAYER(club_id) -- USING MATCH_DETAIL.club_id FOR FOREIGN KEY
+);
+
+CREATE TABLE FOUL_OF(
+	match_id INT UNSIGNED NOT NULL, -- USING UNSIGNED INT, SET FOREIGN KEY
+    club_id INT UNSIGNED NOT NULL, -- USING UNSIGNED TINYINT, SET FOREIGN KEY
+    player_id INT UNSIGNED NOT NULL, -- USING UNSIGNED INT
+    foul_time CHAR(10) NOT NULL, -- USING UNSIGNED TINYINT
+    sent_off BOOLEAN NOT NULL, -- USING BOOLEAN
+    FOREIGN KEY(match_id) REFERENCES MATCH_DETAIL(Match_id), -- USING MATCH_DETAIL.match_id FOR FOREIGN KEY
+    FOREIGN KEY(player_id) REFERENCES PLAYER(player_id), -- USING PLAYER.player_id FOR FOREIGN KEY
+    FOREIGN KEY(club_id) REFERENCES PLAYER(club_id) -- USING MATCH_DETAIL.club_id FOR FOREIGN KEY
+);
